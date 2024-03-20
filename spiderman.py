1fd01ec4bce8017f30ab1119a0e14c7e27def30e
August-iv <138834625+August-iv@users.noreply.github.com>
2024-03-20 16:24:12 +0300

    Add files via upload

---
+++ spiderman.py
@@ -0,0 +1,30 @@
+print("What's your name?: ")
+myName = input()
+if myName == "August":
+    print()
+    print("Welcome Champ!")
+    print()
+    print("You're the sickest producer in the game rn!")
+else:
+    print("Who on earth are you?")
+print()
+print("Are you a cat or a dog person?: ")
+catsOrDogs = input()
+if catsOrDogs == "cat":
+    print()
+    print("Meow")
+else:
+    print("Woof!")
+print()
+print("Time to pick a Marvel Character, 'yes' or 'no' respones")
+print()
+print("Do you like 'hanging around'?:")
+character = input()
+if character == "yes":
+    print("Say Hello to Zendaya for me Spider-man! pretty Please")
+else:
+    print("Just pick yes bruvvvvv. Do you want to be Spider-man or not!!! ")
+    print("coz that's the only option you mf!!!😊.", "\033[31m", "Its my 5th day of coding bruuvvv!!", "\033[0m")
+    print("anyway thanks for taking time to entertain my BS. I love you but only if you agree Ronaldo is the GOAT😏.")
+    print()
+    print("'BOMBASTIC SIDE EYE'👀")
